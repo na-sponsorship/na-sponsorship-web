6af911ba48799cb2ef213e1440beb84f095f9bc3
David Hevesi <davidhevesi2008@gmail.com>
2019-04-11 19:54:50 -0400

    Swiched sections

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -18,38 +18,8 @@ div
                   router-link.btn.btn-xxl.text-uppercase.u-btn-primary.g-font-weight-700.g-font-size-11.g-color-gray-dark-v1.g-color-gray-dark-v1--hover.rounded-0.g-py-10.g-py-20--md.g-px-15.g-px-25--md.g-mr-10(to='/sponsor')
                     FAIcon(icon="heart")
                     |  Sponsor a Child
-                  a.btn.btn-xxl.text-uppercase.u-btn-primary.g-font-weight-700.g-font-size-11.g-color-gray-dark-v1.g-color-gray-dark-v1--hover.rounded-0.g-py-10.g-py-20--md.g-px-15.g-px-25--md(href='#' tabindex='0') Learn more        
-  section.g-py-50
-    .container.g-max-width-780.text-center.g-mb-60
-      .text-center.u-heading-v8-1.g-mb-35
-        h2.h3.text-uppercase.u-heading-v8__title.g-font-weight-700.g-font-size-26.mb-0
-          | How you
-          strong.g-bg-gray-dark-v1.g-color-primary can help
-      p.mb-0
-        | There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don&apos;t look even slightly believable.
-    .container
-      .row
-        .col-lg-6
-          article.row.align-items-stretch.text-center.g-color-gray-dark-v5.g-bg-black.mx-0
-            .col-sm-6.u-ns-bg-v1-bottom.u-ns-bg-v1-right--md.g-bg-black.g-px-30.g-py-45
-              h3.text-uppercase.g-font-size-20.g-color-white.g-mb-25
-                | Became a
-                strong.d-block.g-font-weight-700.g-color-primary Volunteer
-              p.g-mb-25
-                | Praesent pulvinar gravida efficitur. Aenean bibendum purus eu nisi pulvinar venenatis vitae non velit.
-              a.btn.btn-md.text-uppercase.u-btn-primary.g-font-weight-700.g-font-size-11.g-color-gray-dark-v1.g-color-gray-dark-v1--hover.rounded-0.g-py-10.g-px-25(href='#') Join Us
-            .col-sm-6.px-0.u-bg-overlay.g-bg-black-opacity-0_2--after.g-bg-img-hero.g-min-height-300(data-bg-img-src='assets/img-temp/800x842/img1.jpg' style='background-image: url("assets/img-temp/800x842/img1.jpg");')
-        .col-lg-6
-          article.row.align-items-stretch.text-center.g-color-gray-dark-v5.g-bg-black.mx-0
-            .col-sm-6.px-0.u-bg-overlay.g-bg-black-opacity-0_2--after.g-bg-img-hero.g-min-height-300(data-bg-img-src='assets/img-temp/800x842/img2.jpg' style='background-image: url("assets/img-temp/800x842/img2.jpg");')
-            .col-sm-6.u-ns-bg-v1-top.u-ns-bg-v1-left--md.g-bg-black.g-px-30.g-py-45
-              h3.text-uppercase.g-font-size-20.g-color-white.g-mb-25
-                | Make a
-                strong.d-block.g-font-weight-700.g-color-primary Dontation
-              p.g-mb-25
-                | Praesent pulvinar gravida efficitur. Aenean bibendum purus eu nisi pulvinar venenatis vitae non velit.
-              a.btn.btn-md.text-uppercase.u-btn-primary.g-font-weight-700.g-font-size-11.g-color-gray-dark-v1.g-color-gray-dark-v1--hover.rounded-0.g-py-10.g-px-25(href='#') Join Us
-  section.u-bg-overlay.g-bg-black-opacity-0_7--after.g-bg-img-hero.g-py-50
+                  a.btn.btn-xxl.text-uppercase.u-btn-primary.g-font-weight-700.g-font-size-11.g-color-gray-dark-v1.g-color-gray-dark-v1--hover.rounded-0.g-py-10.g-py-20--md.g-px-15.g-px-25--md(href='#' tabindex='0') Learn more
+                  section.u-bg-overlay.g-bg-black-opacity-0_7--after.g-bg-img-hero.g-py-50
     .u-bg-overlay__inner
       .container.g-max-width-780.text-center.g-mb-60
         .text-center.u-heading-v8-1.g-mb-35
@@ -113,7 +83,38 @@ div
                     h4.text-uppercase.g-font-weight-700.g-font-size-10.g-color-primary.g-mb-16 Molestie ullamcorper nisl eget
                     p.g-mb-0
                       | Integer accumsan maximus leo, et consectetur metus vestibulum in. Vestibulum viverra justo odio. Donec eu nulla leo. Vivamus risus lacus, viverra eu maximus non, tincidunt sodales massa. Duis vulputate purus a libero luctus, sed dictum ante interdum. Nam vel leo ultricies, pretium magna at, consequat augue. Quisque bibendum vel enim quis pulvinar. Proin ac quam erat.
-          .js-next.u-arrow-v1.g-pos-abs.g-absolute-centered--x--md.g-top-0.g-width-40.g-height-40.g-color-white.g-color-black--hover.g-bg-black.g-bg-primary--hover.g-transition-0_2.g-transition--ease-in.fa.fa-chevron-right.g-right-0.g-right-50x--md.g-ml-85--md.slick-arrow(style='')
+          .js-next.u-arrow-v1.g-pos-abs.g-absolute-centered--x--md.g-top-0.g-width-40.g-height-40.g-color-white.g-color-black--hover.g-bg-black.g-bg-primary--hover.g-transition-0_2.g-transition--ease-in.fa.fa-chevron-right.g-right-0.g-right-50x--md.g-ml-85--md.slick-arrow(style='')        
+  section.g-py-50
+    .container.g-max-width-780.text-center.g-mb-60
+      .text-center.u-heading-v8-1.g-mb-35
+        h2.h3.text-uppercase.u-heading-v8__title.g-font-weight-700.g-font-size-26.mb-0
+          | How you
+          strong.g-bg-gray-dark-v1.g-color-primary can help
+      p.mb-0
+        | There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don&apos;t look even slightly believable.
+    .container
+      .row
+        .col-lg-6
+          article.row.align-items-stretch.text-center.g-color-gray-dark-v5.g-bg-black.mx-0
+            .col-sm-6.u-ns-bg-v1-bottom.u-ns-bg-v1-right--md.g-bg-black.g-px-30.g-py-45
+              h3.text-uppercase.g-font-size-20.g-color-white.g-mb-25
+                | Became a
+                strong.d-block.g-font-weight-700.g-color-primary Volunteer
+              p.g-mb-25
+                | Praesent pulvinar gravida efficitur. Aenean bibendum purus eu nisi pulvinar venenatis vitae non velit.
+              a.btn.btn-md.text-uppercase.u-btn-primary.g-font-weight-700.g-font-size-11.g-color-gray-dark-v1.g-color-gray-dark-v1--hover.rounded-0.g-py-10.g-px-25(href='#') Join Us
+            .col-sm-6.px-0.u-bg-overlay.g-bg-black-opacity-0_2--after.g-bg-img-hero.g-min-height-300(data-bg-img-src='assets/img-temp/800x842/img1.jpg' style='background-image: url("assets/img-temp/800x842/img1.jpg");')
+        .col-lg-6
+          article.row.align-items-stretch.text-center.g-color-gray-dark-v5.g-bg-black.mx-0
+            .col-sm-6.px-0.u-bg-overlay.g-bg-black-opacity-0_2--after.g-bg-img-hero.g-min-height-300(data-bg-img-src='assets/img-temp/800x842/img2.jpg' style='background-image: url("assets/img-temp/800x842/img2.jpg");')
+            .col-sm-6.u-ns-bg-v1-top.u-ns-bg-v1-left--md.g-bg-black.g-px-30.g-py-45
+              h3.text-uppercase.g-font-size-20.g-color-white.g-mb-25
+                | Make a
+                strong.d-block.g-font-weight-700.g-color-primary Dontation
+              p.g-mb-25
+                | Praesent pulvinar gravida efficitur. Aenean bibendum purus eu nisi pulvinar venenatis vitae non velit.
+              a.btn.btn-md.text-uppercase.u-btn-primary.g-font-weight-700.g-font-size-11.g-color-gray-dark-v1.g-color-gray-dark-v1--hover.rounded-0.g-py-10.g-px-25(href='#') Join Us
+  
 </template>
 
 <script>
