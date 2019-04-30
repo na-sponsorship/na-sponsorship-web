f8887b01f7492f817204d24c95f8417b68aacdf1
David Hevesi <davidhevesi2008@gmail.com>
2019-04-30 06:44:17 -0400

    Changed colors adjusted layout

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="">
-    <div class="bg-blue h-new">red zone</div>
-    <div class="bg-red h-new">red zone</div>
-  </div>
+    <div class="bg-grey-darker h-new"></div>
+    <div class="bg-grey-darker h-new"></div>
+   </div>
 </template>
+
 
 <script>
 import axios from "axios";
-// import BgOverlay from "@components/BgOverlay";
+import BgOverlay from "@components/BgOverlay";
 
 export default {
   components: {
-    // BgOverlay
+    BgOverlay
   },
   data() {
     return {
