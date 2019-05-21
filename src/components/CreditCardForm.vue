269d5c9f086f175ac4896e55095590a48f7711a6
Istoc, Eugen (ei2074) <ei2074@att.com>
2019-05-21 07:55:03 -0400

    add stripe credit card form

--- src/components/CreditCardForm.vue
+++ src/components/CreditCardForm.vue
@@ -1,8 +1,53 @@
 <template>
   <form id="credit-card-payment-form">
     <div class="form-control">
-      <label for="card-element">Credit or Debit Card</label>
+      <label for="card-element">
+        Credit or debit card
+      </label>
       <div id="card-element"></div>
+
+      <div id="card-errors" role="alert"></div>
     </div>
+    <button type="button" class="btn-primary" @click.prevent="submit()">
+      Submit Payment
+    </button>
   </form>
 </template>
+
+<script>
+let card;
+
+export default {
+  mounted() {
+    const elements = this.$Stripe.elements();
+
+    var style = {
+      base: {
+        fontSize: "16px",
+        color: "#32325d"
+      }
+    };
+
+    card = elements.create("card", { style });
+    card.mount("#card-element");
+  },
+  methods: {
+    submit() {
+      this.$Stripe
+        .createToken(card)
+        .then(token => {
+          if (token.error) {
+            console.log(token.error);
+            return;
+          }
+
+          // send token to server
+        })
+        .catch(err => {
+          debugger;
+          console.log(err);
+        });
+    }
+  }
+};
+</script>
