7020f88d544bf04a102d12f4724477ffcde370ac
Istoc, Eugen (ei2074) <ei2074@att.com>
2019-05-25 13:24:50 -0400

    Mockup the account page

--- src/views/pages/Account.view.vue
+++ src/views/pages/Account.view.vue
@@ -1,5 +1,77 @@
 <template>
-  <pre>account here</pre>
+  <div class="flex justify-center">
+    <div class="page-width-contraint">
+      <div class="flex flex-col">
+        <h2>Hello, Sponsor</h2>
+        <div class="flex-1">
+          <h3 class="font-bold">
+            Your information <a href="#" class="text-blue-500">Edit</a>
+          </h3>
+          <div>
+            Info here
+          </div>
+        </div>
+        <div class="flex">
+          <div class="flex-1">
+            <div class="flex flex-col">
+              <div>
+                <h3 class="font-bold">Your Contributions</h3>
+                <ul>
+                  <li>
+                    John Doe ($30.00/month)
+                    <a href="#" class="text-blue-500">Details</a>
+                  </li>
+                  <li>
+                    General Fund ($5.00/month)<a href="#" class="text-blue-500">
+                      Details
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div>
+              <h3 class="font-bold">Payment Method</h3>
+              <span>
+                XXXX-XXXX-XXXX-4242
+              </span>
+              <a href="#" class="text-blue-500">Update</a>
+            </div>
+          </div>
+          <div class="flex-1">
+            <h3 class="font-bold">Recent Transactions</h3>
+            <table>
+              <thead>
+                <tr>
+                  <td class="font-bold">Date</td>
+                  <td class="font-bold">Name</td>
+                  <td class="font-bold">Amount</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>04/21/2019</td>
+                  <td>John Doe Sponsorship</td>
+                  <td>$30.00</td>
+                </tr>
+                <tr>
+                  <td>04/21/2019</td>
+                  <td>General Children's Fund</td>
+                  <td>$5.00</td>
+                </tr>
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colspan="3" class="text-right">
+                    <a href="#" class="text-blue-500">Download Transactions</a>
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="postcss"></style>
