39257d6f0cb126291f953b7f7152bdefc32ac004
David Hevesi <davidhevesi2008@gmail.com>
2019-05-26 10:11:31 -0400

    created login page

--- src/views/pages/Login.view.vue
+++ src/views/pages/Login.view.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="flex justify-center">
-    <div class="page-width-contraint">
-      <pre>Login here</pre>
+  <div class="flex justify-center mb-10 mt-10">
+    <div class="page-width-contraint flex-col shadow-lg p-3">
+    <div class="">
+        <h1 class="font-medium text-4xl">Secure Donation</h1>
+        <p class="text-lg mb-4">Enter your email address to receive your security code.</p>
     </div>
+    <div class="flex">
+      <div>
+        <input class="w-full py-2 px-20" type="text" name="" id="">
+      </div>
+      <div>
+        <button class="bg btn-primary w-full py-2 px-3 mb-2">GET CODE</button>
+      </div>
+    </div>
+
+  </div>
   </div>
 </template>
 
