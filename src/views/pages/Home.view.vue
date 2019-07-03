502a679d4a3d21aeb848f6280bdc52c8522ae4c5
Eugen Istoc <eugenistoc@gmail.com>
2019-07-03 07:41:23 -0400

    update ui

---
+++ src/views/pages/Home.view.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="flex flex-col z-30">
+    <hero>
+      <div>
+        <h1 class="text-3xl text-gray-300 mb-5">Be the reason someone smiles today</h1>
+      </div>
+      <div class="mt-5 mb-5 opacity-75">
+        <span class="text-gray-300 text-base">
+          With your help, we can change the life stories of many
+          children
+        </span>
+      </div>
+      <div>
+        <div class="flex">
+          <div class="flex">
+            <div class="labeled-tag text-gray-400">
+              <span class="fa-lg">
+                <FAIcon :icon="['fas', 'caret-right']"/>
+              </span>
+              Needing Sponsorship
+            </div>
+            <div class="bg-yellow-400 pr-3 pl-3 pt-2">{{ childrenCount }} children</div>
+          </div>
+          <router-link tag="button" class="btn btn-primary ml-5" to="/children">Sponsor Now</router-link>
+        </div>
+      </div>
+    </hero>
+    <div class="flex-1 bg-gray-100 h-new">Content here</div>
+  </div>
+</template>
+
+<style>
+.h-40-rem {
+  height: 40rem;
+}
+
+.lbl::before {
+  @apply border-black;
+}
+</style>
+
+<script>
+import axios from "axios";
+import hero from "../../components/Hero";
+
+export default {
+  components: { hero },
+  data() {
+    return {
+      childrenCount: null,
+    };
+  },
+  created() {
+    axios
+      .get(process.env.VUE_APP_API + "/children/needingSponsorship")
+      .then(res => {
+        this.childrenCount = res.data;
+      });
+  }
+};
+</script>
--- src/views/pages/children/ChildrenList.view.vue
+++ src/views/pages/children/ChildrenList.view.vue
@@ -1,44 +1,35 @@
 <template>
-  <div class="flex justify-center">
-    <div class="page-width-contraint">
-      <div class="flex flex-col">
-        <div class="text-center p-5">
-          <p class="uppercase m-5">
-            How you
-            <span class="bg-black text-white p-2 font-bold">can help</span>
-          </p>
-          <p>More info here</p>
-        </div>
-        <div class="flex flex-wrap">
-          <ChildCard
-            v-for="(child, index) in children"
-            v-bind:key="index"
-            :child="child"
-            @onSponsor="sponsor"
-          ></ChildCard>
+  <div>
+    <hero>
+      hero here
+    </hero>
+    <div class="flex justify-center">
+      <div class="page-width-contraint -mt-40 z-10">
+        <div class="flex flex-col">
+          <div class="flex flex-wrap">
+            <ChildCard
+              v-for="(child, index) in children"
+              v-bind:key="index"
+              :child="child"
+              @onSponsor="sponsor"
+            ></ChildCard>
+          </div>
         </div>
       </div>
     </div>
-
-    <SponsorChildModal
-      :child="selectedChild"
-      @onDismiss="selectedChild = null"
-    ></SponsorChildModal>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-
-import SponsorChildModal from "@components/modals/SponsorChildModal";
-
+import hero from "../../../components/Hero";
 import ChildCard from "@components/ChildCard";
 import { chunk } from "lodash-es";
 
 export default {
   components: {
-    SponsorChildModal,
-    ChildCard
+    ChildCard,
+    hero
   },
   data() {
     return {
