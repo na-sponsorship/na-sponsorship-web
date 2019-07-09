b9cb14b5673c0fa0735456b56c800781fdd5558e
Eugen Istoc <eugenistoc@gmail.com>
2019-07-09 15:00:50 -0400

    update purgecss config to not strip global styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,3 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
-
 module.exports = {
   plugins: [
     require("tailwindcss")("tailwind.config.js"),
@@ -8,7 +6,16 @@ module.exports = {
     process.env.NODE_ENV === "production"
       ? require("@fullhuman/postcss-purgecss")({
           content: ["./src/**/*.html", "./src/**/*.vue"],
-          defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+          extractors: [
+            {
+              extractor: class {
+                static extract(content) {
+                  return content.match(/[A-Za-z0-9-_:/]+/g) || [];
+                }
+              },
+              extensions: ["html", "vue"]
+            }
+          ]
         })
       : ""
   ]
