f16bcca8762a86153f18136be743ea844c502b0f
Eugen Istoc <eugenistoc@gmail.com>
2019-07-09 14:33:02 -0400

    add defaultExtractor to purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,8 @@ module.exports = {
     require("autoprefixer"),
     process.env.NODE_ENV === "production"
       ? require("@fullhuman/postcss-purgecss")({
-          content: ["./src/**/*.html", "./src/**/*.vue"]
+          content: ["./src/**/*.html", "./src/**/*.vue"],
+          defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
         })
       : ""
   ]
