aaa5770fe12a1871c13d6821d5210c168eb97a2c
Eugen Istoc <eugenistoc@gmail.com>
2019-07-09 13:43:19 -0400

    add purgecss to reduct the css bundle size

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+
 module.exports = {
   plugins: [
-    require('tailwindcss')('tailwind.config.js'),
-    require('postcss-nested'),
-    require('autoprefixer')
+    require("tailwindcss")("tailwind.config.js"),
+    require("postcss-nested"),
+    require("autoprefixer"),
+    process.env.NODE_ENV === "production"
+      ? require("@fullhuman/postcss-purgecss")({
+          content: ["./src/**/*.html", "./src/**/*.vue"]
+        })
+      : ""
   ]
 };
