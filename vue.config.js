409d010251cb5390c1c52d8aa59753bb8ce116e7
Eugen Istoc <eugenistoc@gmail.com>
2019-07-09 15:36:39 -0400

    optimie images in production

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,22 @@
-const path = require("path");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const imageminMozjpeg = require("imagemin-mozjpeg");
 
 module.exports = {
   configureWebpack: {
     resolve: {
       alias: require("./aliases.config").webpack
-    }
+    },
+    plugins: [
+      new ImageminPlugin({
+        disable: process.env.NODE_ENV === "production",
+        plugins: [
+          imageminMozjpeg({
+            quality: 75,
+            progressive: true
+          })
+        ]
+      })
+    ]
   },
   css: {
     // Enable CSS source maps.
