862054c2cbf7df6b4afb0eb322ef186a14b1138e
Eugen Istoc <eugenistoc@gmail.com>
2019-07-09 15:18:28 -0400

    fix issue with purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,7 @@ module.exports = {
     require("autoprefixer"),
     process.env.NODE_ENV === "production"
       ? require("@fullhuman/postcss-purgecss")({
-          content: ["./src/**/*.html", "./src/**/*.vue"],
+          content: ["./public/**/*.html", "./src/**/*.vue"],
           defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
         })
       : ""
