6ad52c398212c21a50ef26dacfcddb9865a2cc37
Eugen Istoc <eugenistoc@gmail.com>
2019-07-09 15:00:50 -0400

    update purgecss config to not strip global styles

--- postcss.config.js
+++ postcss.config.js
@@ -5,17 +5,8 @@ module.exports = {
     require("autoprefixer"),
     process.env.NODE_ENV === "production"
       ? require("@fullhuman/postcss-purgecss")({
-          content: ["./src/**/*.html", "./src/**/*.vue"],
-          extractors: [
-            {
-              extractor: class {
-                static extract(content) {
-                  return content.match(/[A-Za-z0-9-_:/]+/g) || [];
-                }
-              },
-              extensions: ["html", "vue"]
-            }
-          ]
+          content: ["./public/**/*.html", "./src/**/*.vue"],
+          defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
         })
       : ""
   ]
