8fde5f2576ca7e86767ca41a617e2d260c8d837d
David Hevesi <davidhevesi2008@gmail.com>
2019-07-19 21:22:44 -0400

    Add background adjusted the contact form

--- src/views/pages/Contact.view.vue
+++ src/views/pages/Contact.view.vue
@@ -1,8 +1,11 @@
 <template>
-<div class="flex justify-center mb-6">
-    <div class="flex flex-col px-16 bg-gray-200 rounded-lg shadow-2xl mb-4 pb-10">
-        <span class="text-green-600 text-4xl font-bold my-3">Contact Form</span>
-        <span class="uppercase text-sm my-4 font-bold text-gray-700">Your Information</span>
+  <div>
+      <div class="bg-auto">
+      <hero :header-bg="bgImage"></hero></div>
+      <div class="flex justify-center mb-6">
+       <div class="flex flex-col px-16 bg-gray-200 rounded-lg shadow-2xl mb-4 pb-10 -mt-40 z-10">
+         <span class="text-green-600 text-4xl font-bold my-3">Contact Form</span>
+         <span class="uppercase text-sm my-4 font-bold text-gray-700">Your Information</span>
             <div class="flex mb-3">
             <div class="flex-1 mr-2 form-group">
                 <input placeholder="First Name" class="form-input w-full">
@@ -20,6 +23,21 @@
          <button type="button" class="btn btn-secondary mt-2 px-3 py-2 text-base float-right">
                   Send
         </button>
+        </div>
     </div>
 </div>
 </template>
+<script>
+import axios from "axios";
+import hero from "../../components/Hero";
+
+export default {
+  components: { hero },
+  data() {
+    return {
+      bgImage: require("@assets/img/headers/children2.jpg"),
+      
+    }
+  }
+ };
+</script>
