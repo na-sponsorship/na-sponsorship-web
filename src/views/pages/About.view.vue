1354902e703beb1d7ec46f9f14b6bc837bee8cf8
David Hevesi <davidhevesi2008@gmail.com>
2019-08-01 13:29:06 -0400

    add buttons

--- src/views/pages/About.view.vue
+++ src/views/pages/About.view.vue
@@ -9,7 +9,10 @@
       <hr class="bg-white h-px opacity-50 w-1/2" />
       <div class="text-sm text-white text-center">
         <h2>
-          A simply dummy text of the pribeen the industry's standard dum .
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque,
+          nesciunt ducimus! Tempore laborum quia ut pariatur aliquid molestiae
+          quasi, provident ipsam, repellat eaque adipisci atque nam quis nulla,
+          nemo amet.
         </h2>
       </div>
     </hero>
@@ -25,8 +28,11 @@
                 <div class="px-5 ">
                   <h2 class="text-xl text-gray-700 font-cursive">Our Vision</h2>
                   <hr class="bg-gray-700 h-px" />
-                  <p class="text-green-700 pb-5 text-3xl">
-                    wewnfnsel;fns;lkslk;fnlaksndlasdiasjdo
+                  <p class="text-green-700 pb-5 text-small">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Minus enim architecto beatae consectetur autem accusantium,
+                    quae quaerat ex dicta ipsa aperiam! Doloremque asperiores
+                    nihil animi hic voluptas obcaecati odit sapiente!
                   </p>
                 </div>
               </div>
@@ -38,19 +44,36 @@
     <div class="flex justify-center">
       <div class="page-width-constraint">
         <div class="flex mb-4 ">
-          <div class="w-1/4 bg-gray-500 h-12"></div>
-          <div class="w-1/4 bg-gray-400 h-12"></div>
+          <div class="w-1/4 h-12">
+            <h1 class="text-3xl text-green-500 font-cursive">Lorem Sertem</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+              unde accusamus doloribus corrupti quas cupiditate? Praesentium
+              cumque quo aspernatur. Quibusdam quos omnis
+            </p>
+          </div>
+          <div class="w-1/4 h-12"></div>
           <div class="w-1/4 shadow-lg bg-white rounded-lg h-12 p-40 mr-4"></div>
           <div class="w-1/4 shadow-lg bg-white rounded-lg h-12 p-40"></div>
         </div>
       </div>
     </div>
-    <div class="bg-green-500 h-screen -mt-16 -mt-16">
+    <div class="bg-green-500 object-cover h-68 w-full -mt-16">
       <div class="flex justify-center">
         <div class="page-width-constraint">
           <div class="flex mb-4 mt-24">
-            <div class="w-1/4 bg-gray-500 h-12"></div>
-            <div class="w-1/4 bg-gray-400 h-12"></div>
+            <div class="w-1/4 h-12">
+              <h1 class="text-2xl text-white font-cursive">Our Team</h1>
+              <p class="text-white">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Officiis deleniti consequuntur minima soluta nisi. Nisi nihil
+                architecto molestias voluptate.
+              </p>
+              <button class="btn btn-primary mt-2 px-2 py-2 text-base mb-8 ">
+                Contact Us
+              </button>
+            </div>
+            <div class="w-1/4 h-12"></div>
             <div
               class="w-1/4 shadow-lg bg-white rounded-lg h-12 p-40 mr-4"
             ></div>
@@ -59,14 +82,14 @@
         </div>
       </div>
     </div>
-    <div class="bg-white-500 h-screen -mt-16 -mt-16">
+    <div
+      class="bg-white-500 object-cover w-full h-cover h-68 mb-64 -mt-10 z-10"
+    >
       <div class="flex justify-center ">
         <div class="page-width-constraint">
           <div class="flex mb-4 pt-40">
-            <div class="w-1/2 h-12">
-              <h1 class="text-3xl text-green-500 text-center font-cursive">
-                Lorem Sertem
-              </h1>
+            <div class="w-1/2 h-12 mr-4">
+              <h1 class="text-3xl text-green-500 font-cursive">Lorem Sertem</h1>
               <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Molestias, distinctio. Unde sit quod in culpa fugit quidem quo
@@ -74,11 +97,11 @@
               </p>
             </div>
             <div class="w-1/2 h-12 pt-10">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
-                unde omnis dolorem consectetur ipsa. Quod hic excepturi omnis
-                facere ad repudiandae quae? Cum fuga, ea corrupti pariatur
-                eveniet obcaecati quae?
+              <p class="">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+                facere corporis officiis laborum fugiat quam vitae magnam quasi
+                laboriosam amet reprehenderit excepturi reiciendis harum, sit
+                cupiditate qui. Nihil, sed dolores!
               </p>
             </div>
           </div>
