d38278885d98817e6a10202157811ba28bb4f502
David Hevesi <davidhevesi2008@gmail.com>
2019-08-01 09:58:12 -0400

    Created new cards

--- src/views/pages/About.view.vue
+++ src/views/pages/About.view.vue
@@ -40,14 +40,51 @@
         <div class="flex mb-4 ">
           <div class="w-1/4 bg-gray-500 h-12"></div>
           <div class="w-1/4 bg-gray-400 h-12"></div>
-          <div class="w-1/4 bg-gray-500 h-12">
-            <div class=""></div>
-          </div>
-          <div class="w-1/4 bg-gray-400 h-12"></div>
+          <div class="w-1/4 shadow-lg bg-white rounded-lg h-12 p-40 mr-4"></div>
+          <div class="w-1/4 shadow-lg bg-white rounded-lg h-12 p-40"></div>
         </div>
       </div>
     </div>
-    <div class="bg-green-500 h-screen"></div>
+    <div class="bg-green-500 h-screen -mt-16 -mt-16">
+      <div class="flex justify-center">
+        <div class="page-width-constraint">
+          <div class="flex mb-4 mt-24">
+            <div class="w-1/4 bg-gray-500 h-12"></div>
+            <div class="w-1/4 bg-gray-400 h-12"></div>
+            <div
+              class="w-1/4 shadow-lg bg-white rounded-lg h-12 p-40 mr-4"
+            ></div>
+            <div class="w-1/4 shadow-lg bg-white rounded-lg h-12 p-40"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-white-500 h-screen -mt-16 -mt-16">
+      <div class="flex justify-center ">
+        <div class="page-width-constraint">
+          <div class="flex mb-4 pt-40">
+            <div class="w-1/2 h-12">
+              <h1 class="text-3xl text-green-500 text-center font-cursive">
+                Lorem Sertem
+              </h1>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias, distinctio. Unde sit quod in culpa fugit quidem quo
+                asperiores distinctio id ipsa.
+              </p>
+            </div>
+            <div class="w-1/2 h-12 pt-10">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+                unde omnis dolorem consectetur ipsa. Quod hic excepturi omnis
+                facere ad repudiandae quae? Cum fuga, ea corrupti pariatur
+                eveniet obcaecati quae?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
