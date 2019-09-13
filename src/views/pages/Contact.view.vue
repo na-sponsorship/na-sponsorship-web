60de73dc88108d3d87b5ae2d1290bb832c4a595f
Eugen Istoc <eugenistoc@gmail.com>
2019-09-13 10:09:59 -0400

    toggle the spinner before the captcha is checeked

--- src/views/pages/Contact.view.vue
+++ src/views/pages/Contact.view.vue
@@ -208,9 +208,9 @@ export default {
   methods: {
     sendMessage(contactForm) {
       this.$v.$touch();
+      this.isSending = true;
       this.$recaptcha("contact").then(token => {
         if (!this.$v.$invalid) {
-          this.isSending = true;
           axios
             .post(`${process.env.VUE_APP_API}/app/contact`, contactForm, {
               headers: { recaptcha: token }
